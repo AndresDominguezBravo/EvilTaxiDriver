3249963e30f5f3ea0698a636f0cfe66243a2a2fe
Andrés Domínguez Bravo <32138386+AndresDominguezBravo@users.noreply.github.com>
2021-08-23 15:01:09 +0200

    Implementación de movimiento por teclado

--- JS/main.js
+++ JS/main.js
@@ -4,25 +4,28 @@ let mapTablero = new Map();
 let Marcadores = {
     dinero: 0,
     numClientes: 0,
-    distancia:0,
+    distancia: 0,
 }
+let ultimoMovimiento = "";
+let intervalo = 0;
 
-function start() {
-    document.getElementsByClassName("menuPrincipal")[0].remove();
+function start(isPrimeraRonda) {
+    if (isPrimeraRonda) {
+        document.getElementsByClassName("menuPrincipal")[0].remove();
+        document.getElementsByClassName("score")[0].style.display = 'block';
+        document.getElementById("controles").style.display = 'inline-block';
+    } else {
+        document.getElementsByClassName("tablero")[0].innerHTML = "";
+    }
     generarMundoDisplay();
     elementCasillasToMap();
-    generarEdificosPorCasilla(tipoGeneracionCasilla.CLIENTE_MAS_TAXI,false);
+    generarEdificosPorCasilla(tipoGeneracionCasilla.CLIENTE_MAS_TAXI, false);
     generarClienteMasTaxi();
     generarEventoCasilla();
-}
-function nextLevel() {
-    document.getElementsByClassName("tablero")[0].innerHTML= "";
- 
-    generarMundoDisplay();
-    elementCasillasToMap();
-    generarEdificosPorCasilla(tipoGeneracionCasilla.CLIENTE_MAS_TAXI,false);
-    generarClienteMasTaxi();
-    generarEventoCasilla();
+    generarEventoTeclado();
+    document.getElementById("MarcadorDinero").textContent = Marcadores.dinero;
+    document.getElementById("MarcadorClientes").textContent = Marcadores.numClientes;
+    document.getElementById("MarcadorTiempo").textContent = nuevaCuentaAtras();
 }
 
 const tipoCasilla = {
@@ -35,15 +38,14 @@ const tipoCasilla = {
 }
 
 const tipoGeneracionCasilla = {
-    CLIENTE_MAS_TAXI:1,
-    EDIFICIOS:2
+    CLIENTE_MAS_TAXI: 1,
+    EDIFICIOS: 2
 }
 
 
 function generarMundoDisplay() {
 
     var divCasilla = document.createElement("div");
-
     var tablero = document.getElementsByClassName("tablero");
     var fila = document.createElement("div");
     fila.classList.add("fila");
@@ -83,7 +85,7 @@ function isEdificio() {
     }
 }
 
-function generarEdificosPorCasilla(tipoGeneracionCasilla,modoLog) {
+function generarEdificosPorCasilla(tipoGeneracionCasilla, modoLog) {
     for (var columna = 1; columna < numeroCasillas; columna++) {
         for (var fila = 1; fila < numeroCasillas; fila++) {
             coordenada = columna + "-" + fila;
@@ -100,85 +102,167 @@ function generarEdificosPorCasilla(tipoGeneracionCasilla,modoLog) {
             let contadorEdificios = 0;
             let contadorEdificiosConectados = 0;
             CordenadasVecinas.allVecinas.forEach(c => {
-                if(c!==undefined){
+                if (c !== undefined) {
                     elementoDomCasilla = mapTablero.get(c);
-                    if(elementoDomCasilla.classList.contains(tipoCasilla.EDIFICIO)){
+                    if (elementoDomCasilla.classList.contains(tipoCasilla.EDIFICIO)) {
                         contadorEdificios++;
                     }
-                }else{
+                } else {
                     contadorEdificios++;
                 }
 
             });
             CordenadasConectadas.allConectadas.forEach(c => {
-                if(c!==undefined){
+                if (c !== undefined) {
                     elementoDomCasilla = mapTablero.get(c);
-                    if(elementoDomCasilla.classList.contains(tipoCasilla.EDIFICIO)){
+                    if (elementoDomCasilla.classList.contains(tipoCasilla.EDIFICIO)) {
                         contadorEdificiosConectados++;
                     }
-                }else{
+                } else {
                     contadorEdificiosConectados++;
                 }
 
             });
 
             //modo desarrollador
-            if(modoLog){
-                if(contadorEdificios <=3){
-                    if(coordenada.classList.contains(tipoCasilla.CARRETERA)){ 
-                        coordenada.textContent = contadorEdificios +"-"+ contadorEdificiosConectados;
-                        coordenada.classList.add("numEdificio-"+contadorEdificios);
+            if (modoLog) {
+                if (contadorEdificios <= 3) {
+                    if (coordenada.classList.contains(tipoCasilla.CARRETERA)) {
+                        coordenada.textContent = contadorEdificios + "-" + contadorEdificiosConectados;
+                        coordenada.classList.add("numEdificio-" + contadorEdificios);
                     }
                 }
             }
-            
-            switch(tipoGeneracionCasilla){
+
+            switch (tipoGeneracionCasilla) {
                 case 2:
-                    if(coordenada.classList.contains(tipoCasilla.CARRETERA)){               
-                        if(contadorEdificios <= 1){
+                    if (coordenada.classList.contains(tipoCasilla.CARRETERA)) {
+                        if (contadorEdificios <= 1) {
                             coordenada.classList.add(tipoCasilla.EDIFICIO);
                             coordenada.classList.remove(tipoCasilla.CARRETERA);
-                        }               
-                    }
-                    break;
-                case 1:
-                    if(contadorEdificios <=3){
-                        if(coordenada.classList.contains(tipoCasilla.CARRETERA)){                             
-                           coordenada.classList.add(tipoCasilla.CARRETERA_VALIDA);
                         }
                     }
                     break;
-            }          
+                case 1:
+                    if (contadorEdificios <= 3) {
+                        if (coordenada.classList.contains(tipoCasilla.CARRETERA)) {
+                            coordenada.classList.add(tipoCasilla.CARRETERA_VALIDA);
+                        }
+                    }
+                    break;
+            }
         }
     }
 }
 
-function generarClienteMasTaxi(){
+function generarClienteMasTaxi() {
     let listadoCarretas = document.getElementsByClassName(tipoCasilla.CARRETERA_VALIDA);
     listadoCarretas[0].classList.add(tipoCasilla.CLIENTE);
     listadoCarretas[listadoCarretas.length - 1].classList.add(tipoCasilla.TAXI);
 
 }
 
-function generarEventoCasilla(){
-    let listadoCarretas =Array.prototype.slice.call(document.getElementsByClassName(tipoCasilla.CARRETERA));
+function generarEventoCasilla() {
+    let listadoCarretas = Array.prototype.slice.call(document.getElementsByClassName(tipoCasilla.CARRETERA));
     listadoCarretas.forEach(element => {
         element.addEventListener("mouseover", añadirEvento);
-        element.addEventListener("touchmove", añadirEvento);
+        //element.addEventListener("touchmove", añadirEvento);
     });
 
 
 }
 
-function añadirEvento(e){
+function generarEventoTeclado() {
+
+    document.addEventListener("keydown", leerTecla, false);
+    //document.addEventListener("keyup", keyUpHandler, false);
+
+    function leerTecla(e) {
+        let movimiento;
+        if (e.keyCode == 40 || e.keyCode == 83) {
+            movimiento = "abajo";
+        } else if (e.keyCode == 39 || e.keyCode == 68) {
+            movimiento = "derecha";
+        } else if (e.keyCode == 38 || e.keyCode == 87) {
+            movimiento = "arriba";
+        } else if (e.keyCode == 37 || e.keyCode == 65) {
+            movimiento = "izquierda";
+        }
+
+        movimientoPerpetuoTaxi(movimiento);
+    }
+
+}
+
+function movimientoPerpetuoTaxi(movimiento) {
+
+    if (ultimoMovimiento !== movimiento) {
+        moverTaxi(movimiento);
+        if (intervalo <= 1) {
+            intervalo = setInterval(moverTaxi, 200, movimiento);
+        }
+    }
+
+
+}
+
+function moverTaxi(movimiento) {
+
+    let auxTaxi = document.getElementsByClassName("taxi")[0];
+
+    let datosTaxi = cordenadasConectadasByCordenada(auxTaxi.dataColumna, auxTaxi.dataFila);
+
+    switch (movimiento) {
+        case "arriba":
+            logicaMoverTaxi(datosTaxi.arriba, auxTaxi);
+            break;
+        case "abajo":
+            logicaMoverTaxi(datosTaxi.abajo, auxTaxi);
+            break;
+        case "derecha":
+            logicaMoverTaxi(datosTaxi.derecha, auxTaxi);
+            break;
+        case "izquierda":
+            logicaMoverTaxi(datosTaxi.izquierda, auxTaxi);
+            break;
+    }
+
+    ultimoMovimiento = movimiento;
+}
+
+function logicaMoverTaxi(nextCoordenadas, auxTaxi) {
+    if (isMovimientoValido(mapTablero.get(nextCoordenadas))) {
+        if (!mapTablero.get(nextCoordenadas).classList.contains(tipoCasilla.CLIENTE)) {
+            mapTablero.get(nextCoordenadas).classList.add(tipoCasilla.TAXI);
+            auxTaxi.classList.remove(tipoCasilla.TAXI);
+            auxTaxi.classList.add(tipoCasilla.RUTA);
+        } else {
+            cargarNuevaRonda();
+        }
+    }
+}
+
+function isMovimientoValido(nextCoordenadas) {
+    if (nextCoordenadas != undefined) {
+        if (!nextCoordenadas.classList.contains(tipoCasilla.EDIFICIO)) {
+            return true;
+        }
+    } else {
+        clearInterval(intervalo);
+        return false;
+    }
+}
+
+function añadirEvento(e) {
+    clearInterval(intervalo);
     element = e.target;
-    if(element.classList.contains(tipoCasilla.CARRETERA)){
+    if (element.classList.contains(tipoCasilla.CARRETERA)) {
         let CordenadasConectadas = cordenadasConectadasByCordenada(element.dataColumna, element.dataFila);
         let isRutaOTaxi = false;
         CordenadasConectadas.allConectadas.forEach(c => {
             let casilla = mapTablero.get(c);
-            if(c!==undefined){
-                if(casilla.classList.contains(tipoCasilla.TAXI)){
+            if (c !== undefined) {
+                if (casilla.classList.contains(tipoCasilla.TAXI)) {
                     isRutaOTaxi = true;
                     casilla.classList.remove(tipoCasilla.TAXI);
                     casilla.classList.add(tipoCasilla.RUTA);
@@ -186,26 +270,33 @@ function añadirEvento(e){
             }
 
         });
-        if(isRutaOTaxi){
-            if(element.classList.contains(tipoCasilla.CLIENTE)){
+        if (isRutaOTaxi) {
+            if (element.classList.contains(tipoCasilla.CLIENTE)) {
                 cargarNuevaRonda();
-            }   
+            }
             element.classList.add(tipoCasilla.TAXI);
         }
-        
+
     }
-    
+
 
 }
 
-function cargarNuevaRonda(){
-
-    Marcadores.distancia += document.getElementsByClassName(tipoCasilla.RUTA).length * unidadCasillasMetros;
-    Marcadores.numClientes 
-    = ++Marcadores.numClientes;
-    Marcadores.dinero += Marcadores.distancia * 0.002;
+function cargarNuevaRonda() {
+    clearInterval(intervalo);
+    let distancia = document.getElementsByClassName(tipoCasilla.RUTA).length * unidadCasillasMetros;
+    Marcadores.distancia += distancia * unidadCasillasMetros;
+    Marcadores.numClientes = ++Marcadores.numClientes;
+    Marcadores.dinero = Marcadores.dinero + Math.floor(distancia * 0.002);
+    document.getElementById("MarcadorDinero").textContent = Marcadores.dinero;
+    document.getElementById("MarcadorClientes").textContent = Marcadores.numClientes;
+    document.getElementById("MarcadorTiempo").textContent = nuevaCuentaAtras();
     console.log(Marcadores);
-    nextLevel();
+    start(false);
+}
+
+function nuevaCuentaAtras() {
+    return 60;
 }
 
 function cordenadasVencinasByCordenada(columna, fila) {
@@ -222,14 +313,14 @@ function cordenadasVencinasByCordenada(columna, fila) {
         allVecinas: [],
     }
     CoordenadasVecinas.allVecinas = [CoordenadasVecinas.arriba,
-                                    CoordenadasVecinas.arribaDerecha,
-                                    CoordenadasVecinas.derecha,
-                                    CoordenadasVecinas.abajoDerecha,
-                                    CoordenadasVecinas.abajo,
-                                    CoordenadasVecinas.abajoIzquierda,
-                                    CoordenadasVecinas.izquierda,
-                                    CoordenadasVecinas.arribaIzquierda
-                                    ]
+        CoordenadasVecinas.arribaDerecha,
+        CoordenadasVecinas.derecha,
+        CoordenadasVecinas.abajoDerecha,
+        CoordenadasVecinas.abajo,
+        CoordenadasVecinas.abajoIzquierda,
+        CoordenadasVecinas.izquierda,
+        CoordenadasVecinas.arribaIzquierda
+    ]
     return CoordenadasVecinas;
 }
 
@@ -246,7 +337,7 @@ function cordenadasConectadasByCordenada(columna, fila) {
         CoordenadasConectadas.derecha,
         CoordenadasConectadas.abajo,
         CoordenadasConectadas.izquierda,
-                                    ]
+    ]
     return CoordenadasConectadas;
 }
 
@@ -267,7 +358,5 @@ function elementCasillasToMap() {
         }
     }
     console.log(mapTablero);
-    generarEdificosPorCasilla(tipoGeneracionCasilla.EDIFICIOS,false);
+    generarEdificosPorCasilla(tipoGeneracionCasilla.EDIFICIOS, false);
 }
-
-start();
